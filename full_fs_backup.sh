07ad9457d4f58d49e93f1af9984494d2ae059e57
Chris Scheible <cgscheible@yahoo.com>
2017-08-15 06:53:28 +0800

    full_fs_backup.sh - updated parameters

--- full_fs_backup.sh
+++ full_fs_backup.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 # see: https://wiki.archlinux.org/index.php/Full_system_backup_with_rsync
 
+# vars
 PARAM=$1
 DATE=$(date +%Y%m%d)
-LOG="log/rsync-errors.$(hostname -s).${DATE}.log"
+LOG_PATH="log"
+LOG="${LOG_PATH}/rsync-errors.$(hostname -s).${DATE}.log"
+RC=0
+
+# check for log path
+if [[ ! -d ${LOG_PATH} ]]; then
+  mkdir -pv ${LOG_PATH}
+  RC=$?
+fi
 
 if [[ -z $PARAM ]]; then
   echo ""
@@ -13,13 +22,21 @@ if [[ -z $PARAM ]]; then
   echo ""
   echo "see ${LOG} for transfer errors"
   echo ""
-else
+  RC=1
+fi
+
+if [[ $RC -eq 0 ]]; then
   rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/usb","/media/*","/lost+found"} / ${PARAM} 2> ${LOG}
+  RC=$?
   echo "==> rsync transfer completed."
   echo ""
-  echo "==> transfer error log follows ..."
-  echo "==> logfile: ${LOG}"
-  echo ""
-  cat ${LOG}
-  echo ""
+  if [[ $RC -ne 0 ]]; then 
+    echo "==> transfer error log follows ..."
+    echo "==> logfile: ${LOG}"
+    echo ""
+    cat ${LOG}
+    echo ""
+  fi
 fi
+
+exit ${RC}
